88beb94e6a161339037df55e969eafbc3e3d740a
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-02-26 17:28:35 +0100

    mzFileWrite method String

--- src/MZFileHandler.java
+++ src/MZFileHandler.java
@@ -1,3 +1,19 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MZFileHandler {
      
+    /**
+     * <h3>My File Write</h3> Overloaded
+     * @param <Thing>
+     * @param fileName file amibe szeretne írni
+     * @param fileWrite amit szeretne beleírni a {@code fileName} -be
+     * @throws IOException
+     */
+    static <Thing> void mzFileWrite(String fileName, Thing fileWrite)
+    throws IOException{
+        FileWriter file = new FileWriter(fileName);
+        file.write(String.valueOf(fileWrite));
+        file.close();
+    }
 }
